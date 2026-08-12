ae852162bd08f7effd1ddadabd9589dc4eb85e98
ishanik10-ai <ishan.ik10@gmail.com>
2026-08-12 22:38:51 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Paste your deployed Google Apps Script Web App URL here.
-const GOOGLE_API_URL = 'https://script.google.com/macros/s/AKfycbx3nj4Z7weXasgGUvbv1wRgR1xJgLNNLdtY0JJMOvfdSCX6R20ZftChMOWCUw8XH1kx4g/exec';
+const GOOGLE_API_URL = 'https://script.google.com/macros/s/AKfycbzfKLnNVFQXRykQegB7R7tXCRuc4Ul4sfYyYd2Fkh5sxrwDlRtK3sdG7WD5RXUYJdiygA/exec';
 // Must exactly match API_KEY in GoogleAppsScript.gs.
 const GOOGLE_API_KEY = 'IshanPOS-2026-9xK7pQ4mZ8vN2';
