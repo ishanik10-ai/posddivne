83060b1e5b395f16ec4edcf87b0fea425940e69e
ishanik10-ai <ishan.ik10@gmail.com>
2026-08-12 22:22:17 +0600

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Paste your deployed Google Apps Script Web App URL here.
-const GOOGLE_API_URL = 'https://script.google.com/macros/s/AKfycbx3nj4Z7weXasgGUvbv1wRgR1xJgLNNLdtY0JJMOvfdSCX6R20ZftChMOWCUw8XH1kx4g/exec';
-// Must exactly match API_KEY in GoogleAppsScript.gs.
-const GOOGLE_API_KEY = 'IshanPOS-2026-9xK7pQ4mZ8vN2';
